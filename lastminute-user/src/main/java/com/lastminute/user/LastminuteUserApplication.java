e5b5581717baadbe52fccd46225eae9ae41e933f
김형기 <khk211113@naver.com>
2023-03-30 13:24:33 +0900

    chore : user 모듈 세팅

---
+++ lastminute-user/src/main/java/com/lastminute/user/LastminuteUserApplication.java
@@ -0,0 +1,13 @@
+package com.lastminute.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastminuteUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastminuteUserApplication.class, args);
+    }
+
+}
---
+++ lastminute-user/src/test/java/com/lastminute/user/LastminuteUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastminute.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastminuteUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
