f751abd12b675a134e81c01adc66c3fde1ea97af
jycforest29 <jycforest29.dev@gmail.com>
2023-04-25 12:44:59 +0900

    chore: payment 모듈 세팅

---
+++ lastminute-payment/src/main/java/com/lastminute/payment/LastminutePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.lastminute.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastminutePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastminutePaymentApplication.class, args);
+	}
+
+}
---
+++ lastminute-payment/src/test/java/com/lastminute/payment/LastminutePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastminute.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastminutePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
